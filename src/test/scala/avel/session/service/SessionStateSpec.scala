369cd76fe0abb5ade5fa76c8822144664df70663
Pavel <pavel.oliynyk@gmail.com>
2025-08-18 17:31:40 +0100

    test: fix test :: step 2 [temp]

--- src/test/scala/avel/session/service/SessionStateSpec.scala
+++ src/test/scala/avel/session/service/SessionStateSpec.scala
@@ -13,20 +13,31 @@ class SessionStateSpec extends CatsEffectSuite {
 
   implicit val logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
 
-    test("HelloWorld returns status code 200") {
+    test("SessionService :: returns status code 200") {
       assertIO(retSessionState.map(_.status) ,Status.Ok)
     }
-  //
-  //  test("HelloWorld returns hello world message") {
-  //    assertIO(retHelloWorld.flatMap(_.as[String]), "{\"message\":\"Hello, world\"}")
-  //  }
 
-  private[this] val retSessionState: IO[Response[IO]] = {
+
+    test("SessionService::INC returns status code 200") {
+      assertIO(retSessionStateInc.map(_.status) ,Status.Ok)
+      //TODO: fix values for the test
+      //assertIO(retSessionStateInc.flatMap(x => x.as[String]), "{\"message\":\"Hello, world\"}")
+    }
+
+  private[this] def retSessionState: IO[Response[IO]] = {
     val getHW = Request[IO](Method.GET, uri"/session")
     for {
       state <- SessionStateCounter.impl[IO]
-      r <- SessionStateRoutes[IO](state).routes().orNotFound(getHW)
-    } yield r
+      request <- SessionStateRoutes[IO](state).routes().orNotFound(getHW)
+    } yield request
+  }
+
+  private[this] def retSessionStateInc: IO[Response[IO]] = {
+    val getHW = Request[IO](Method.GET, uri"/session/inc")
+    for {
+      state <- SessionStateCounter.impl[IO]
+      request <- SessionStateRoutes[IO](state).routes().orNotFound(getHW)
+    } yield request
   }
 
 }
