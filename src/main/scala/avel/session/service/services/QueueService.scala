ac925a6a80cf959b8e5af409ed297ca10554e304
Pavel <pavel.oliynyk@gmail.com>
2025-08-20 11:31:27 +0100

    fix: support empty result from the queue

--- src/main/scala/avel/session/service/services/QueueService.scala
+++ src/main/scala/avel/session/service/services/QueueService.scala
@@ -3,26 +3,32 @@ package avel.session.service.services
 import cats.effect.kernel.Async
 import cats.effect.std.Queue
 import cats.implicits._
+import org.typelevel.log4cats.Logger
 
 trait QueueService[F[_]] {
-  def pull: F[String]
+  def pull: F[Option[String]]
   def push(item: String): F[Unit]
 }
 
 
 object QueueService {
-  val queueSize : Int = 10
+  private val queueSize : Int = 10
 
-  def imp[F[_]: Async] : F[QueueService[F]] = {
+  def imp[F[_]: Async: Logger] : F[QueueService[F]] = {
 
     Queue.bounded[F, String](queueSize).map { queue =>
       new QueueService[F] {
 
-        override def pull: F[String] = {
-          queue.take
+        override def pull: F[Option[String]] = {
+          queue.tryTake.flatMap{ value =>
+            Logger[F].info(s"QueueService::PULL: $value") *>
+            value.pure[F]
+          }
+
         }
 
         override def push(item: String): F[Unit] = {
+          Logger[F].info(s"QueueService::PUSH->$item") *>
           queue.offer(item)
         }
 
