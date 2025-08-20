784c68e814a0a24cd2e1dc017c3f99621b0c6bd2
Pavel <pavel.oliynyk@gmail.com>
2025-08-20 17:01:22 +0100

    feat: suppress error on the Route level

--- src/main/scala/avel/session/service/routes/ErrorRoute.scala
+++ src/main/scala/avel/session/service/routes/ErrorRoute.scala
@@ -1,7 +1,7 @@
 package avel.session.service.routes
 
 import cats.effect.kernel.Sync
-import cats.implicits.catsSyntaxApplyOps
+import cats.implicits.{catsSyntaxApplicativeError, catsSyntaxApplyOps}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
@@ -13,9 +13,11 @@ final case class ErrorRoutes[F[_] :Sync: Logger]() extends Http4sDsl[F] {
   private[routes] val prefixPath = "/error"
 
   val errorRoute: HttpRoutes[F] = HttpRoutes.of[F] {
-    case GET -> Root =>
+    case GET -> Root / "suppression" =>
       Logger[F].error("Exception raised ...") *>
         Sync[F].delay(throw new Exception("Hey don't swallow me") )
+          .attempt *>
+      Ok("Ok")
   }
 
   def routes(): HttpRoutes[F] = Router(
