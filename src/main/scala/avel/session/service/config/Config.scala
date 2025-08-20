6473211e8a0d7bbfcdebf43b1939bc9c64e0eca2
Pavel <pavel.oliynyk@gmail.com>
2025-08-20 09:38:03 +0100

    refactor: drop comment

--- src/main/scala/avel/session/service/config/Config.scala
+++ src/main/scala/avel/session/service/config/Config.scala
@@ -7,7 +7,6 @@ import pureconfig.generic.auto._
 
 object Config {
 
-  // Ciris promotes configuration as code
   def load[F[_] : Sync]: F[Option[ServiceConfig]] = {
     ConfigSource.default.load[ServiceConfig].toOption.pure[F]
   }
