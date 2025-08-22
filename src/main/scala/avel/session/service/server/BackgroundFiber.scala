8dffd969bad640e8a5c2b3caea339bdef21cd11e
Pavel <pavel.oliynyk@gmail.com>
2025-08-22 17:25:47 +0100

    refactor: create BackgroundFiber trait [2]

--- src/main/scala/avel/session/service/server/BackgroundFiber.scala
+++ src/main/scala/avel/session/service/server/BackgroundFiber.scala
@@ -3,13 +3,17 @@ package avel.session.service
 import avel.session.service.services.SessionService
 import cats.effect.IO
 
+import scala.concurrent.duration.FiniteDuration
+
 trait BackgroundFiber {
 
   import scala.concurrent.duration.DurationInt
 
+  private val refreshFrequency: FiniteDuration = 10.second
+
   def backgroundProcess(sessionService: SessionService[IO]): IO[Unit] = {
     for {
-      _ <- IO.sleep(10.second)
+      _ <- IO.sleep(refreshFrequency)
       _ <- IO.cede
       _ <- sessionService.cleanUp
       _ <- IO.cede
