7a6186f07bd4357a3a23000309e9b769962720d1
Pavel <pavel.oliynyk@gmail.com>
2025-08-22 15:50:41 +0100

    feat: session auto-clean up

--- src/main/scala/avel/session/service/services/SessionService.scala
+++ src/main/scala/avel/session/service/services/SessionService.scala
@@ -3,23 +3,25 @@ package avel.session.service.services
 import avel.session.service.models.UserSessionData
 import cats.effect.kernel.{Async, Sync, Temporal}
 import cats.effect.std.{MapRef, Queue}
-import cats.implicits.{catsSyntaxApplyOps, catsSyntaxFlatten, toFlatMapOps, toFunctorOps}
+import cats.implicits.{catsSyntaxApplicativeId, catsSyntaxApplyOps, catsSyntaxFlatMapOps, catsSyntaxIfM, toFlatMapOps, toFunctorOps}
 import org.typelevel.log4cats.Logger
 
 import scala.concurrent.duration.DurationInt
 
 
 trait SessionService[F[_]] {
-  def create(session: UserSessionData) : F[Unit]
+  def create(session: UserSessionData): F[Unit]
+
   def getById(sessionId: String): F[Option[UserSessionData]]
+
   def cleanUp: F[Unit]
 }
 
 object SessionService {
-  private val shardCound : Int = 5
-  private val queueSize : Int = 100
+  private val shardCound: Int = 5
+  private val queueSize: Int = 100
 
-  def impl[F[_] : Async: Logger]: F[SessionService[F]] = {
+  def impl[F[_] : Async : Logger]: F[SessionService[F]] = {
     Queue.bounded[F, String](queueSize).map { queue =>
       MapRef.ofShardedImmutableMap[F, String, UserSessionData](shardCound).map { mapRef =>
         new SessionService[F] {
@@ -30,23 +32,29 @@ object SessionService {
           }
 
           override def getById(sessionId: String): F[Option[UserSessionData]] = {
-            mapRef(sessionId).update(ss => ss.map(_.tick).flatten) *>
-              mapRef(sessionId).get
+            mapRef(sessionId).updateAndGet(ss =>
+              ss.map(_.tick).flatten
+            ) *> mapRef(sessionId).get
           }
 
           // Clean up not expired sessions
           override def cleanUp: F[Unit] = {
             Temporal[F].delay(10.second) *>
-              Sync[F].delay{
-                for {
-                 sessionId <- queue.take
-                  res = getById(sessionId)
-                } yield res.flatMap ( _.map(_ => queue.offer(sessionId)).get )
-
-              } *> queue.size.flatMap(size =>
+              Sync[F].delay(
+                queue.tryTake.flatMap { sessionId =>
+                  (sessionId.pure[F]).map(_.isDefined).ifM(
+                    getById(sessionId.get).map(_.isEmpty).ifM(
+                      Logger[F].info(s"Sessions: drop: $sessionId"),
+                      Logger[F].info(s"Sessions: still alive: $sessionId") *>
+                        queue.tryOffer(sessionId.getOrElse("")).void
+                    ),
+                    Logger[F].info("Sessions: None")
+                  )
+                }).flatten >>
+              queue.size.flatMap(size =>
                 Logger[F].info(s"CleanUp: sessions::Total: ${size}"))
-
           }
+
         }
       }
     }.flatten
